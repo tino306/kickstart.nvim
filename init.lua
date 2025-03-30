0736a7029ea87189a5b12f3770254d462c1f444c
tino <lehtinen.tino@outlook.com>
2025-03-30 15:43:31 +0300

    added a venv location for basedpyright

--- init.lua
+++ init.lua
@@ -167,11 +167,11 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float, { desc = 'Display diagnostic message' })
 vim.keymap.set('n', '<leader>dy', function()
-  local line = vim.api.nvim_win_get_cursor(0)[1] -- Get the current line number
+  local line = vim.api.nvim_win_get_cursor(0)[1]                 -- Get the current line number
   local diagnostics = vim.diagnostic.get(0, { lnum = line - 1 }) -- Get diagnostics for the current line
   if #diagnostics > 0 then
-    local message = diagnostics[1].message -- Get the first diagnostic message
-    vim.fn.setreg('+', message) -- Copy to the system clipboard
+    local message = diagnostics[1].message                       -- Get the first diagnostic message
+    vim.fn.setreg('+', message)                                  -- Copy to the system clipboard
     print('Copied diagnostic message: ' .. message)
   else
     print 'No diagnostic message on this line.'
@@ -281,7 +281,7 @@ require('lazy').setup({
   -- Then, because we use the `opts` key (recommended), the configuration runs
   -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
 
-  { -- Useful plugin to show you pending keybinds.
+  {                     -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     opts = {
@@ -327,7 +327,7 @@ require('lazy').setup({
 
       -- Document existing key chains
       spec = {
-        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>c', group = '[C]ode',     mode = { 'n', 'x' } },
         { '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
@@ -367,7 +367,7 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -475,7 +475,7 @@ require('lazy').setup({
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim',       opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
@@ -629,9 +629,9 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        gopls = {},
+        -- gopls = {},
         basedpyright = {},
-        rust_analyzer = {},
+        -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -986,3 +986,7 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+
+-- Add basedpyright venv location
+vim.g.python3_host_prog = '/home/tino/.venvs/basedpyright/bin/python'
