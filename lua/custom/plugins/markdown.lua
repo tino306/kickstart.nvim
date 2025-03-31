2079ffe8b4dc6f67cc8b9753fffd27f0768069cd
tino <lehtinen.tino@outlook.com>
2025-03-31 13:12:44 +0300

    removed nvim-metals.lua and neo-tree.lua, removed obsidian plugin from makrdown.lua

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,49 +1,49 @@
 vim.opt_local.conceallevel = 2
 return {
   -- obsidian
-  {
-    'epwalsh/obsidian.nvim',
-    version = '*',
-    lazy = true,
-    ft = 'markdown',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    opts = {
-      workspaces = {
-        {
-          name = 'Obsidian Vault',
-          path = '/Users/tino/Documents/Obsidian Vault',
-        },
-      },
-      ui = { enable = false },
-    },
-    -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
-    -- way then set 'mappings = {}'.
-    mappings = {
-      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-      ['gf'] = {
-        action = function()
-          return require('obsidian').util.gf_passthrough()
-        end,
-        opts = { noremap = false, expr = true, buffer = true },
-      },
-      -- Toggle check-boxes.
-      ['<leader>ch'] = {
-        action = function()
-          return require('obsidian').util.toggle_checkbox()
-        end,
-        opts = { buffer = true },
-      },
-      -- Smart action depending on context, either follow link or toggle checkbox.
-      ['<cr>'] = {
-        action = function()
-          return require('obsidian').util.smart_action()
-        end,
-        opts = { buffer = true, expr = true },
-      },
-    },
-  },
+  -- {
+  --   'epwalsh/obsidian.nvim',
+  --   version = '*',
+  --   lazy = true,
+  --   ft = 'markdown',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --   },
+  --   opts = {
+  --     workspaces = {
+  --       {
+  --         name = 'Obsidian Vault',
+  --         path = '/Users/tino/Documents/Obsidian Vault',
+  --       },
+  --     },
+  --     ui = { enable = false },
+  --   },
+  --   -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+  --   -- way then set 'mappings = {}'.
+  --   mappings = {
+  --     -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+  --     ['gf'] = {
+  --       action = function()
+  --         return require('obsidian').util.gf_passthrough()
+  --       end,
+  --       opts = { noremap = false, expr = true, buffer = true },
+  --     },
+  --     -- Toggle check-boxes.
+  --     ['<leader>ch'] = {
+  --       action = function()
+  --         return require('obsidian').util.toggle_checkbox()
+  --       end,
+  --       opts = { buffer = true },
+  --     },
+  --     -- Smart action depending on context, either follow link or toggle checkbox.
+  --     ['<cr>'] = {
+  --       action = function()
+  --         return require('obsidian').util.smart_action()
+  --       end,
+  --       opts = { buffer = true, expr = true },
+  --     },
+  --   },
+  -- },
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
