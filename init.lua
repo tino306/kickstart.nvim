212484b3a03e5e4d28e641d06e17ed2e83e8c050
Tino Lehtinen <tino.lehtinen@aalto.fi>
2025-10-09 15:43:19 +0300

    added oil.nvim and revamped markdown.nvim config

--- init.lua
+++ init.lua
@@ -135,7 +135,7 @@ vim.opt.signcolumn = 'yes'
 vim.opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
+vim.opt.timeoutlen = 800
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,10 +1,22 @@
 vim.opt_local.conceallevel = 2
+vim.keymap.set('n', '<leader>on', ':ObsidianNew<CR>', { desc = 'Obsidian: new note' })
+vim.keymap.set('n', '<leader>oo', ':ObsidianQuickSwitch<CR>', { desc = 'Obsidian: open / switch note' })
+vim.keymap.set('n', '<leader>os', ':ObsidianSearch<CR>', { desc = 'Obsidian: search notes' })
+vim.keymap.set('n', '<leader>ot', ':ObsidianToday<CR>', { desc = "Obsidian: open today's note" })
+vim.keymap.set('n', '<leader>oy', ':ObsidianYesterday<CR>', { desc = "Obsidian: open yesterday's note" })
+vim.keymap.set('n', '<leader>or', ':ObsidianRename<CR>', { desc = 'Obsidian: rename note' })
+vim.keymap.set('n', '<leader>ol', ':ObsidianLinkNew<CR>', { desc = 'Obsidian: link to new note' })
+vim.keymap.set('n', '<leader>op', ':ObsidianPasteImg<CR>', { desc = 'Obsidian: paste image' })
+vim.keymap.set('n', '<leader>od', function()
+  local vault_path = vim.fn.expand '~/nas/00_vault/'
+  vim.cmd('e' .. vim.fn.fnameescape(vault_path))
+end, { desc = 'Obsidian: open vault in directory editor' })
 return {
-  -- obsidian
+  -- obsidian.nvim
   {
     'epwalsh/obsidian.nvim',
     version = '*',
-    lazy = true,
+    lazy = false,
     ft = 'markdown',
     dependencies = {
       'nvim-lua/plenary.nvim',
@@ -12,18 +24,94 @@ return {
     opts = {
       workspaces = {
         {
-          name = 'vault',
-          path = '~/projects/vault',
-        },
-        {
-          name = 'masters-thesis',
-          path = '~/projects/masters_thesis',
+          name = '00_vault',
+          path = '~/nas/00_vault/',
         },
       },
       ui = { enable = false },
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = '00_daily-notes',
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = '%Y-%m-%d',
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = '%B %-d, %Y',
+        -- Optional, default tags to add to each new daily note created.
+        default_tags = { 'daily-notes' },
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = nil,
+      },
+      -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+      completion = {
+        -- Set to false to disable completion.
+        nvim_cmp = true,
+        -- Trigger completion at 2 chars.
+        min_chars = 3,
+      },
+      -- Where to put new notes. Valid options are
+      --  * "current_dir" - put new notes in same directory as the current buffer.
+      --  * "notes_subdir" - put new notes in the default notes subdirectory.
+      -- new_notes_location = "notes_subdir",
+      picker = {
+        -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+        name = 'telescope.nvim',
+        -- Optional, configure key mappings for the picker. These are the defaults.
+        -- Not all pickers support all mappings.
+        note_mappings = {
+          -- Create a new note from your query.
+          new = '<C-x>',
+          -- Insert a link to the selected note.
+          insert_link = '<C-l>',
+        },
+        tag_mappings = {
+          -- Add tag(s) to current note.
+          tag_note = '<C-x>',
+          -- Insert a tag at the current location.
+          insert_tag = '<C-l>',
+        },
+      },
+      -- Specify how to handle attachments.
+      attachments = {
+        img_folder = '99_assets',
+
+        -- Optional, customize the default name or prefix when pasting images via `:ObsidianPasteImg`.
+        ---@return string
+        img_name_func = function()
+          -- Prefix image names with timestamp.
+          return string.format('%s-', os.time())
+        end,
+
+        -- A function that determines the text to insert in the note when pasting an image.
+        -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+        -- This is the default implementation.
+        ---@param client obsidian.Client
+        ---@param path obsidian.Path the absolute path to the image file
+        ---@return string
+        img_text_func = function(client, path)
+          path = client:vault_relative_path(path) or path
+          return string.format('![%s](%s)', path.name, path)
+        end,
+      },
+      -- Optional, customize how note IDs are generated given an optional title.
+      ---@param title string|?
+      ---@return string
+      note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will be given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+        local suffix = ''
+        if title ~= nil then
+          -- If title is given, transform it into valid file name.
+          suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+        else
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        return tostring(os.time()) .. '-' .. suffix
+      end,
     },
-    -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
-    -- way then set 'mappings = {}'.
     mappings = {
       -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
       ['gf'] = {
@@ -51,10 +139,11 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    opts = {
+      bullet = { icons = { '*', '>', '>', '>' } },
+      latex = { enabled = false },
+    },
   },
 }
